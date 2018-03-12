224746b159aaf1d6d56fffc2d8c03c42d6eed36f
Ivan <hi-ban@hotmail.com>
2018-03-12 02:21:08 +0100

    Initial commit (3)

---
+++ build_gcw0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BDAT=$(date +"%Y%m%d-%H%M%S")
+
+echo '#define BUILDDATE "'$BDAT'"' >./gambatte_sdl/builddate.h
+
+echo "cd libgambatte && scons"
+(cd libgambatte && scons -Q target=gcw0) || exit
+
+echo "cd gambatte_sdl && scons"
+(cd gambatte_sdl && scons -Q target=gcw0)
+
+rm -f gambatte-gcw0-r572-$BDAT.opk
+
+mksquashfs default.gcw0.desktop ./gambatte_sdl/gambatte_sdl gambatte.png manual.txt gambatte-gcw0-r572-$BDAT.opk -all-root -no-xattrs -noappend -no-exports
