59103abacf1a4bf00e4e669ef361dc072abda4e9
bardeci <hi-ban@hotmail.com>
2020-06-28 15:31:52 +0200

    Added RG350M build. Added FFWD "Toggle" hotkey option.
    RG350M build features three extra scalers which take advantage of the
    higher resolution screen.

---
+++ build_rg350m.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BDAT=$(date +"%Y%m%d-%H%M%S")
+echo '#define BUILDDATE "'$BDAT'"' >./gambatte_sdl/builddate.h
+
+echo "cd libgambatte && scons"
+(cd libgambatte && scons -Q target=rg350m) || exit
+echo "cd gambatte_sdl && scons"
+(cd gambatte_sdl && scons -Q target=rg350m)
+mv gambatte_sdl/gambatte_sdl gambatte_sdl/gambatte-dms.opendingux
+
+rm -f gambatte-dms-rg350m-r572u4-$BDAT.opk
+
+mksquashfs ./dist/gcw0/default.gcw0.desktop ./gambatte_sdl/gambatte-dms.opendingux ./dist/gcw0/gambatte_dms.png ./dist/gcw0/manual.txt gambatte-dms-rg350m-r572u4-$BDAT.opk -all-root -no-xattrs -noappend -no-exports
+
+echo "cd gambatte_sdl && scons -c"
+(cd gambatte_sdl && scons -c)
+echo "cd libgambatte && scons -c"
+(cd libgambatte && scons -c)
+echo "rm -f *gambatte*/config.log"
+rm -f *gambatte*/config.log
+echo "rm -rf *gambatte*/.scon*"
+rm -rf *gambatte*/.scon*
+find . -type f -iname \*.o -delete
+find . -type f -iname gambatte_sdl -delete
+
+find . -type f -iname gambatte-dms.opendingux -delete
