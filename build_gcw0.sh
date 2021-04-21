2adcea17d7494ea05f759fbc60c4ffe5d98770cc
bardeci <hi-ban@hotmail.com>
2021-04-21 04:02:59 +0200

    Added several upscalers (for devices which support them)
    -Added several upscalers (for devices which support them).
    -Merged RG350 and RG350M versions.
    -Simplified several functions.
    -Removed some redundant files.
    -Updated some build scripts.

---
+++ build_gcw0.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BDAT=$(date +"%Y%m%d-%H%M%S")
+echo '#define BUILDDATE "'$BDAT'"' >./gambatte_sdl/builddate.h
+
+echo "cd libgambatte && scons"
+(cd libgambatte && scons -Q target=gcw0) || exit
+echo "cd gambatte_sdl && scons"
+(cd gambatte_sdl && scons -Q target=gcw0)
+mv gambatte_sdl/gambatte_sdl gambatte_sdl/gambatte-dms.gcw0
+
+echo "cd gambatte_sdl && scons -c"
+(cd gambatte_sdl && scons -c)
+echo "cd libgambatte && scons -c"
+(cd libgambatte && scons -c)
+echo "rm -f *gambatte*/config.log"
+rm -f *gambatte*/config.log
+echo "rm -rf *gambatte*/.scon*"
+rm -rf *gambatte*/.scon*
+find . -type f -iname \*.o -delete
+find . -type f -iname gambatte_sdl -delete
+
+rm -f gambatte-dms-gcw0-r572u4-$BDAT.opk
+mksquashfs ./dist/gcw0/default.gcw0.desktop ./gambatte_sdl/gambatte-dms.gcw0 ./dist/gcw0/gambatte_dms.png ./dist/gcw0/manual.txt gambatte-dms-gcw0-r572u4-$BDAT.opk -all-root -no-xattrs -noappend -no-exports
+
+find . -type f -iname gambatte-dms.gcw0 -delete
